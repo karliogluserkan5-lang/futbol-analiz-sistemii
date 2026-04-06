a49a3411cdad33042a366a6070c2a4df6f7c99bf
karliogluserkan5-lang <karliogluserkan5@gmail.com>
2026-04-07 01:02:24 +0300

    Create team_id_map.py with team ID mappings
    Added a mapping of team names to Sportmonks team IDs.

---
+++ team_id_map.py
@@ -0,0 +1,13 @@
+# Takım ismi -> Sportmonks team ID
+TEAM_IDS = {
+    "Udinese": 129,
+    "Como": 1234,
+    "Portsmouth": 1235,
+    "Oxford United": 1236,
+    "Millwall": 1237,
+    "Norwich City": 1238,
+    "Lecce": 1239,
+    "Atalanta": 1240,
+    "Bristol City": 1241,
+    "Sheffield United": 1242,
+}
